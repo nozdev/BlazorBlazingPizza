24392a157df4f585f440baa769130ed43982b52b
Sam Gentiile <managedcode44@hotmail.com>
2019-10-31 13:54:04 -0400

    Added delete of pizza

---
+++ BlazingPizza.Client/JSRuntimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Client
+{
+    public static class JSRuntimeExtensions
+    {
+        public static ValueTask<bool> Confirm(this IJSRuntime jSRuntime, string message)
+        {
+            return jSRuntime.InvokeAsync<bool>("confirm", message);
+        }
+    }
+}
